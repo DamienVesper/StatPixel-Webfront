a8e464dcc56422f9337513b386afe2ae5f3ab123
Damien Vesper <ldamienvesper@gmail.com>
2021-07-10 14:31:21 -0500

    header: insert basic bs navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,42 @@ import { hot } from 'react-hot-loader';
 
 import '../../public/assets/scss/components/header.scss';
 
+// import Logo from '../../public/assets/img/logo.png'
+
 /**
  * The header of the website.
  */
 class Header extends React.Component {
     render = () => {
         return (
-            <main></main>
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-dark">
+                    <div className="container-fluid">
+                        <a href="/" className="navbar-brand">
+                            {/* <img src={Logo} alt="StatPixel logo" className="header-logo" /> */}
+                            StatPixel
+                        </a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a href="/about" className={`nav-link ${window.location.pathname === `/about` ? `active disabled` : ``}`}>About</a>
+                                </li>
+                            </ul>
+                            {/* <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a href="/tos" className={`nav-link ${window.location.pathname === `/tos` ? `active disabled` : ``}`}>ToS</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="/privacy-policy" className={`nav-link ${window.location.pathname === `/privacy-policy` ? `active disabled` : ``}`}>Privacy Policy</a>
+                                </li>
+                            </ul> */}
+                        </div>
+                    </div>
+                </nav>
+            </header>
         );
     }
 }
